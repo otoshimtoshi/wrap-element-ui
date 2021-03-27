7fafffabff110b0fb5cf159dca7f98ab14515c88
otoshimtoshi <1083otiket@gmail.com>
2021-03-27 23:11:40 +0900

    v0.1.7

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../lib'),
     publicPath: '.',
-    filename: 'main.js',
+    filename: 'wrap-element-ui.js',
     libraryExport: 'default',
     library: 'WrapElementPlus',
     libraryTarget: 'commonjs2',
